7da6525094d3cf981323d11dcd5980a5fe731799
diana.silaraja@gmail.com <diana.silaraja@gmail.com>
2017-12-19 21:02:57 +0200

    hello name

--- FirstRCS/HelloWorld/Program.cs
+++ FirstRCS/HelloWorld/Program.cs
@@ -11,7 +11,6 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
-            Console.ReadLine();
             string myName;
             myName = Console.ReadLine();
             Console.WriteLine("Hello World " + myName);

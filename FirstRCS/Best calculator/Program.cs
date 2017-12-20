48c309a68ca3e0e29aa6e71dfec1e818e49978a3
diana.silaraja@gmail.com <diana.silaraja@gmail.com>
2017-12-20 20:08:02 +0200

    calculation

---
+++ FirstRCS/Best calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Best_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //izveido kalkulēšanas objektu 
+            //new norāda uz jaunu objektu šajā konkrētajā klasē un attiecas uz visu, kas tiek kodēts zemāk
+            calculations calc;
+            calc = new calculations();
+
+            //paprasīt lietotajam vērtību
+            //string nomaina uz int, jo string norādā mainīgo tekstam, 
+            //bet cipariem visur string vietā jānomaina uz int, savādāk, ciparus nevis saskaita, bet savieno
+           int firstNumber = calc.AskUserForNumber();
+
+            //jāpaprasa otra vērtība
+           int secondNumber = calc.AskUserForNumber();
+
+            //saskaita abas vērtības
+            int result = firstNumber + secondNumber;
+            Console.WriteLine("Congratulation! Your result is:");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstRCS/Best calculator/calculations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Best_calculator
+{
+    class calculations
+    {
+        public int AskUserForNumber()
+        {
+            //izvadīt tekstu, kas paprasa ciparu
+            Console.WriteLine("please enter number:");
+            //ielasit ciparu no konsoles
+            //izveidojam mainīgo, kas glabās tekstu un ierakstam mainīgajā to, ko lietotājs uzrakstīs
+            int number;
+            string inputText = Console.ReadLine();
+            //pārveidot lietotāja tekstu par ciparu
+            //int32 ir priekš cipariem
+            //Parse saprot tikai ciparus, ja ievada tekstu, tad aplikācija nobrūk
+            //lai nenobruktu, var izvēlēties TryParse un ievadīt bool success, kam dot funkciju, parveidot tekstu par ciparu
+            //bool nozīmē patiess vai nepatiess
+            bool success = Int32.TryParse(inputText, out number);
+
+            // divas == dod funkciju, lai salīdzinātu, jo, ja viss sakrīt, tad ziņojums neparādās
+            if(success == false)
+            {
+                Console.WriteLine("sorry, please write number");
+                //lai turpinatu funkciju nevis apstādinātu, un liktu lietotājam vēlreiz ievadīt ciparu, lai viss turpinātos, ir jāievada sekojoši:
+                number = this.AskUserForNumber();
+
+            }
+            return number;
+            
+        }
+    }
+}

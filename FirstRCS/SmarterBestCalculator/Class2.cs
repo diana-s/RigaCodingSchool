6449d427e1facab84bc6e81b75207f7db1266693
diana.silaraja@gmail.com <diana.silaraja@gmail.com>
2017-12-26 23:01:19 +0200

    second class added

---
+++ FirstRCS/SmarterBestCalculator/Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmarterBestCalculator
+{
+    class Class2
+    {
+        public int Cycle2(string input)
+        {
+            var FirstEnteredNumber2 = "";
+            var SecondEnteredNumber2 = "";
+            char EnteredOperation2 = ' ';
+            bool OperationFound2 = false;
+
+            int counter = 0;
+            while (counter < input.Length)
+            {
+                char sub = input[counter];
+                if (sub != '-')
+                {
+                    EnteredOperation2 = sub;
+                    OperationFound2 = false;
+                }
+                else
+                {
+                    if (OperationFound2 == false)
+                    {
+                        FirstEnteredNumber2 = FirstEnteredNumber2 + sub;
+
+                    }
+                    else
+                    {
+                        SecondEnteredNumber2 = SecondEnteredNumber2 + sub;
+                    }
+                }
+
+                counter = counter + 1;
+
+
+                if (OperationFound2 == true && counter == input.Length)
+                {
+                    var result2 = int.Parse(FirstEnteredNumber2) - int.Parse(SecondEnteredNumber2);
+                    return result2;
+                }
+
+            }
+            return 0;
+        }
+    }
+}
+
--- FirstRCS/SmarterBestCalculator/Program.cs
+++ FirstRCS/SmarterBestCalculator/Program.cs
@@ -14,14 +14,29 @@ namespace SmarterBestCalculator
             Class1 mathcycle;
             mathcycle = new Class1();
 
+             Class2 matchcycle2;
+            matchcycle2 = new Class2();
+
             //paprasīt lietotājam ievadīt ievadi
             Console.WriteLine("please enter operation");
             string input = Console.ReadLine();
 
-            int result = mathcycle.Cycle1(input);               
-            Console.WriteLine("your result is = "+ result);
-            Console.ReadLine();
-        
+            int result1 = mathcycle.Cycle1(input);
+            int result2 = matchcycle2.Cycle2(input);
+
+            if (result1 == mathcycle.Cycle1(input))
+            {
+                Console.WriteLine("your result is = " + result1);
+                Console.ReadLine();
+            }
+            if(result2 == matchcycle2.Cycle2(input))
+               {
+                    Console.WriteLine("your result is = " + result2);
+                    Console.ReadLine();
+                }
+
+                    
+                 
         }
     }
 }

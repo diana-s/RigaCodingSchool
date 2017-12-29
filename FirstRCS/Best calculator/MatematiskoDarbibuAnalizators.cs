20ef91fad72d5302eced90502ffc548ca14bbf15
diana.silaraja@gmail.com <diana.silaraja@gmail.com>
2017-12-29 17:06:03 +0200

    ByListUpdates show Item list automaticly

--- FirstRCS/Best calculator/MatematiskoDarbibuAnalizators.cs
+++ FirstRCS/Best calculator/MatematiskoDarbibuAnalizators.cs
@@ -11,17 +11,17 @@ namespace Best_calculator
         //funkcija, kas saņem lietotāja ievadītu tekstu un saparsē to, veic matemātiskās darbības
         //un atgriež rezultātu
         public int ParseMath()
-    
+
         {
-             //1+5  (3 vienibas darbībā, izejošā pozīcija ir 0tā pozīcija (nulles punkts))
+            //1+5  (3 vienibas darbībā, izejošā pozīcija ir 0tā pozīcija (nulles punkts))
             //izveidojam mainīgo, kur glabāt rezultatu ar int result
             int result;
-        //counters ir cik ievadītās vienības mēs atļaujam ievadīt. Mēs gribam tikai 3 vienības darbībā.
-        int counter = 0;
+            //counters ir cik ievadītās vienības mēs atļaujam ievadīt. Mēs gribam tikai 3 vienības darbībā.
+            int counter = 0;
 
-             //šeit nodefinējam, ka sistēma pārbaudīs katru vienību līdz 3 vienībām un tad beigs parbaudes ciklu un varam dot nakamo komandu piem. aprēķinat rezultatu.
-             //counter < input.Length var vadīt tik ciparus, cik vien grib, un sistēma apstrādās visas ievadītās vienības.
-            while (counter<input.Length)
+            //šeit nodefinējam, ka sistēma pārbaudīs katru vienību līdz 3 vienībām un tad beigs parbaudes ciklu un varam dot nakamo komandu piem. aprēķinat rezultatu.
+            //counter < input.Length var vadīt tik ciparus, cik vien grib, un sistēma apstrādās visas ievadītās vienības.
+            while (counter < input.Length)
             {
                 //char ir string vai int vieta, ja run air par simbolu
                 char symbol = input[counter];
@@ -30,16 +30,17 @@ namespace Best_calculator
                 {
                     Console.WriteLine("plus");
                 }
-                else 
+                else
                 {
                     //saglabā simbolu teksta virknē, lai tam var piekļūt, kad tiek veikta darbība
                     int number;
-    //Parse funkcija neņem pretī simbolu, tapēc ar ToString mēs iedodam signālu, pārvērst simbolu par tekstu, un tad viss ok
-    number = Int32.Parse(symbol.ToString());
+                    //Parse funkcija neņem pretī simbolu, tapēc ar ToString mēs iedodam signālu, pārvērst simbolu par tekstu, un tad viss ok
+                    number = Int32.Parse(symbol.ToString());
                     Console.WriteLine("number" + number);
                 }
 
-counter = counter + 1;
+                counter = counter + 1;
+            }
         }
     }
 }
--- FirstRCS/BuyList/MainWindow.xaml.cs
+++ FirstRCS/BuyList/MainWindow.xaml.cs
@@ -1,9 +1,16 @@
 ﻿using System.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace BuyList
 {
     using System.Collections.ObjectModel;
-    using System.IO;
+ 
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -14,17 +21,42 @@ namespace BuyList
         //Ja mēs nodefinējam vienkārši "List" tad logā neparadas nekas jauns, tikai, tas kas ir kodā, ja 
         //ievadam textboksā jaunu tekstu, viņš pazūd
         //tapēc vadam "ObservableCollection", kas ir tas pats, kas List, tikai uzreiz parāda izmaiņas.
+
+       
+        
         public ObservableCollection<string> BuyItemsList = new ObservableCollection<string>();
 
         public MainWindow()
         {
             InitializeComponent();
             BuyListItemName.Text = "";
-            this.BuyItemsList.Add("āboli");
-            // pasakamkontrolē i laiizmanto mūsu sarakstu
-            this.BuyItemsList.Add("bumbieri");
 
+            //pievienoju mainīgo, kas ir texta fails, kur saglabājās pirkumu saraksts
+            
+            var text = System.IO.File.ReadAllText(@"C:\Users\Diana\CODES\RigaCodingSchool.test.txt");
+
+            // pasakamkontrolē i laiizmanto mūsu sarakstu
+            //šajā sarakstā norādam, lai automātiski parādās bumbieri (rakstot šajā.mainīgajā.Add();)
+            this.BuyItemsList.Add("bumbieri");
+            //tagad norādam, lai sarakstā parādās automātiski arī, tas, kas tika saglabāts iepriekš 
+            //(ja saraksts tiek veidots pirmo reizi, tad tur noteikti parādīsies tukšums)
+            //kas saglabājas failā. Iekavās rakstam mainīgo, kas šajā gadījumā ir "text"
+            BuyItemsList.Add(text);
+
+
+            //KOMENTĀRS PRIEKŠ MANIS :)"hmmm būtu forši izveidot kodu, kad lietotajam uzsākot darbu, automātiskis izveidojas savs personigais
+            //saraksta fails, kas saglabājas projekta mapē, un tur arī vienmēr ievadās, visi viņa dati.
+            //Droši vien šim vajadzētu izveidot profila izveides iespējas, un tad sistēma automatiski saglaba konkrētā lietotāja datus..."
+
+            //tāpat rakstam mainīgā - kas ir šis bloks, kur parādās saraksts - lai pievieno izveidoto sarakstu
             BuyItemListControl.ItemsSource = BuyItemsList;
+            for (int i = 0; i < text.Length; i++)
+            {
+                //xxx ir jauns mainīgais, kurš ir atsevišķi no visa kopējā saraksta un var viņu ieklikšķināt
+                var xxx = text[i];
+            }
+
+
         }
 
         private void AddListItemButton_Click(object sender, RoutedEventArgs e)
@@ -54,12 +86,7 @@ namespace BuyList
             //i ir counters tikai īsāk nosaukts
             //no sākuma sistēma skata i=0, tad tiek pie i<Return....., kad šis izpildās, sistēma izpilda, to, kas zemā sistēmā minēts
             //kad viss izdarīts tad veic darbību i++, kas ir i = i+1
-            for(int i = 0; i < ReturnFromFile.Length; i++)
-            {
-                //xxx ir jauns mainīgais, kurš ir atsevišķi no visa kopējā saraksta un var viņu ieklikšķināt
-                var xxx = ReturnFromFile[i];
-                this.BuyItemsList.Add(xxx);
-            }
+           
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
@@ -69,5 +96,11 @@ namespace BuyList
 
            
         }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
